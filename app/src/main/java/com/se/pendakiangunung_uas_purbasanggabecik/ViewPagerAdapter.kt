cc1c7151df9322de61b686c3d03cf8f0a5418877
cikssX <sanggabecik@gmail.com>
2022-05-30 12:21:59 +0700

    test 1

---
+++ app/src/main/java/com/se/pendakiangunung_uas_purbasanggabecik/ViewPagerAdapter.kt
@@ -0,0 +1,40 @@
+package com.se.pendakiangunung_uas_purbasanggabecik
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.se.pendakiangunung_uas_purbasanggabecik.FragmentPeralatan
+import com.se.pendakiangunung_uas_purbasanggabecik.FragmentTips
+
+/**
+ * Created by Azhar Rivaldi on 03-06-2021
+ * Youtube Channel : https://bit.ly/2PJMowZ
+ * Github : https://github.com/AzharRivaldi
+ * Twitter : https://twitter.com/azharrvldi_
+ * Instagram : https://www.instagram.com/azhardvls_
+ * Linkedin : https://www.linkedin.com/in/azhar-rivaldi
+ */
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = FragmentPeralatan()
+            1 -> fragment = FragmentTips()
+        }
+        return fragment!!
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        var title = ""
+        when (position) {
+            0 -> title = "Peralatan"
+            1 -> title = "Tips"
+        }
+        return title
+    }
+}
